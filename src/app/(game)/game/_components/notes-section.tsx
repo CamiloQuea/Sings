41a1a4167ab03039712bc086835cdf4cfc50ea19
Camilo <camiloquea98@gmail.com>
2024-03-11 00:28:42 -0500

    feat: drawing song notes predefined

--- src/app/(game)/game/_components/notes-section.tsx
+++ src/app/(game)/game/_components/notes-section.tsx
@@ -1,66 +1,84 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { MicProcessor } from "../_config/mic-processor";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { NoteMicProcessor } from "../_config/note-mic-processor";
+import useIsMounted from "@/hooks/useIsMounted";
 
 type Status = {
   ok: boolean;
   freq: number;
 };
 
+const getFpsMs = (fps: number) => {
+  return 1000 / fps;
+};
+
 const NotesSection = () => {
-  const [micProcessor, setMicProcessor] = useState<MicProcessor | null>(null);
+  const micNoteRef = useRef<NoteMicProcessor>();
+
+  const isMounted = useIsMounted();
 
   const [status, setStatus] = useState<Status | null>(null);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
+    if (isMounted) {
+      initializeGraph();
+      return () => {
+        micNoteRef.current = undefined;
+      };
+    }
+  }, [isMounted]);
+  const value = String(status?.freq);
+
+  const initializeGraph = () => {
+    if (micNoteRef.current) {
+      return;
+    }
+
+    micNoteRef.current = new NoteMicProcessor();
+
     const canvas = canvasRef.current;
     const canvasContext = canvas?.getContext("2d");
 
     if (!canvas || !canvasContext) return;
 
-    if (!micProcessor) {
-      setMicProcessor(new MicProcessor());
-      return;
-    }
+    const micNote = micNoteRef.current.init();
 
-    micProcessor?.init((getPitch) => {
-      const drawAlt = function () {
-        const value = getPitch();
+    const drawAlt = function () {
+      const value = micNote.getPitch();
 
-        const width = canvas.width;
-        const height = canvas.height;
+      const width = canvas.width;
+      const height = canvas.height;
+      // console.log(value);
+      const isSimilar =
+        micNoteRef.current?.noteIsSimilarEnough(value, 130.81, 20) || false;
 
-        const isSimilar =
-          micProcessor?.noteIsSimilarEnough(value, 130.81, 20) || false;
+      const frequency = Number(value.toFixed());
 
-        const frequency = Number(value.toFixed());
+      canvasContext?.clearRect(0, 0, width, height);
 
-        canvasContext?.clearRect(0, 0, width, height);
+      canvasContext.fillStyle = "black";
+      canvasContext.fillRect(0, 0, width, height);
 
-        canvasContext.fillStyle = "rgb(0, 0, 0)";
-        canvasContext.fillRect(0, 0, width, height);
+      var barWidth = width;
+      var barHeight = height - (frequency % height);
+      var x = 0;
 
-        var barWidth = width;
-        var barHeight = height - (frequency % height);
-        var x = 0;
+      canvasContext.fillStyle = "rgb(" + (frequency * 0.4 + 50) + ",50,50)";
+      canvasContext.fillRect(x, barHeight, barWidth, 20);
 
-        canvasContext.fillStyle = "rgb(" + (frequency * 0.4 + 50) + ",50,50)";
-        canvasContext.fillRect(x, barHeight, barWidth, 20);
+      setStatus({
+        freq: frequency,
+        ok: isSimilar,
+      });
+    };
+    setInterval(() => {
+      drawAlt();
+    }, getFpsMs(60));
+  };
 
-        setStatus({
-          freq: frequency,
-          ok: isSimilar,
-        });
-      };
-      setInterval(() => {
-        drawAlt();
-      }, 1000 / 30);
-    });
-  }, [micProcessor]);
-  const value = String(status?.freq);
   return (
     <div>
       <div className="rounded h-20 min-w-0  w-full overflow-hidden  border">
@@ -69,6 +87,7 @@ const NotesSection = () => {
       <div className="text-center">
         {value === "-1" || value === "undefined" ? "-" : value}
       </div>
+      {/* <button onClick={start}>start</button> */}
     </div>
   );
 };
